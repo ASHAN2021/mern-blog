e54e1a5ec4825e97dcde9488b6862ccd7bb6e27b
ASHAN2021 <akh8130@gmail.com>
2024-05-15 11:38:19 +0530

    complete sign in page functionallity

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -35,7 +35,7 @@ export const signin = async(req,res,next)=>{
         if(!validUser){
            return  next(errorHandler(404,'user not found'));
         }
-        const validpassword = bcryptjs.compareSync(password,validUser.password);
+        const validpassword = bcrypt.compareSync(password,validUser.password);
         if(!validpassword){
             return next(errorHandler(400,'invaild password'));
         }
