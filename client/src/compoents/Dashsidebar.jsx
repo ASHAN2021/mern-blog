e821775f6b24b7bf6034b9bbd4b754229372b611
ASHAN2021 <akh8130@gmail.com>
2024-05-22 12:16:20 +0530

    complete user image upload functionallity

--- client/src/compoents/Dashsidebar.jsx
+++ client/src/compoents/Dashsidebar.jsx
@@ -20,7 +20,7 @@ export default function Dashsidebar() {
       <Sidebar.Items>
           <Sidebar.ItemGroup>
             <Link  to='/Dashboard?tab=profile'>
-            <Sidebar.Item active={tab === 'profile'} icon={HiUser} label={'User'} labelColor='dark'> 
+            <Sidebar.Item active={tab === 'profile'} icon={HiUser} label={'User'} labelColor='dark' as='div'> 
                 profile
             </Sidebar.Item>
             </Link>
