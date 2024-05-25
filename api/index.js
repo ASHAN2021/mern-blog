059a38760914ea25cba0939cb04ffd5dff374c61
ASHAN2021 <akh8130@gmail.com>
2024-05-25 17:52:43 +0530

    add edit functionallity in the comment

--- api/index.js
+++ api/index.js
@@ -55,5 +55,5 @@ app.use((err, req, res, next) => {
 });
 
 app.listen(3000, () => {
-    console.log('Server is running on port 3000!!!');
+    console.log('Server is running on port 3000');
 });
--- client/src/compoents/CommentSection.jsx
+++ client/src/compoents/CommentSection.jsx
@@ -82,6 +82,15 @@ export default function CommentSection({postId}) {
         }
       };
 
+      const handleEdit = async (comment, editedContent) => {
+        setComments(
+          comments.map((c) =>
+            c._id === comment._id ? { ...c, content: editedContent } : c
+          )
+        );
+      };
+
+
   return (
 
     <div className='max-w-2xl mx-auto w-full p-3'>
@@ -150,7 +159,7 @@ export default function CommentSection({postId}) {
             key={comment._id}
             comment={comment}
             onLike={handleLike}
-              
+              onEdit={handleEdit}
             />
           ))}
         </>
